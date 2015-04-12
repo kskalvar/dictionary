62dcce67b06ab2da0b84f21942a388fa0c8756a2
osistlk <osistlk@gmail.com>
2015-04-12 11:33:50 -0400

    Read data + simple insert
    Finally able to read data well and performed some sloppy inserts

--- public/core.js
+++ public/core.js
@@ -1,7 +1,22 @@
-var app = angular.module('sample_app', []);
-function sampleController($scope, $http) {
-	$http.get("http://localhost/api/sample").success(function(response) 
-		alert(response.records);
-		$scope.data = response.records;
+var app = angular.module('sampleApp', []);
+app.controller('sampleCtrl', function($scope, $http) {
+	$scope.formData = {};
+	
+	$http.get('/api/sample').success(function(response){
+		$scope.sampleData = response;
+	}).error(function(response) {
+		alert("Error: " + response);
 	});
+	
+	$scope.createSample = function() {
+		$http.post('/api/sample', $scope.formData)
+		.success(function(data){
+			$scope.formData = {};
+			$scope.sampleData = data;
+			console.log(data);
+		})
+		.error(function(data) {
+			console.error('Error: ' + data);
+		});
+	}
 });
