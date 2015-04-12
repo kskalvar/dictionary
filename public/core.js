b7c448f0dbab91ff0366029757d04da7c78184ec
osistlk <osistlk@gmail.com>
2015-04-12 10:45:10 -0400

    Finally getting some data back from the backend, unsure how to parse it in angular

--- public/core.js
+++ public/core.js
@@ -1,1 +1,7 @@
-core.js
+var app = angular.module('sample_app', []);
+function sampleController($scope, $http) {
+	$http.get("http://localhost/api/sample").success(function(response) 
+		alert(response.records);
+		$scope.data = response.records;
+	});
+});
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-mongoose.connect('mongodb://localhost//dictionary');
+mongoose.connect('mongodb://localhost/dict');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback) {console.log("connection success");});
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -13,6 +16,66 @@ app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
+
+var atrbSchema = mongoose.Schema({
+	_id: Number,
+	name: String
+});
+
+var atrb = mongoose.model('sample', atrbSchema, "sample");
+
+app.get('/api/sample', function(req, res) {
+	var query = atrb.find();
+	query.exec(function(err, row) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(row);
+		}
+	});
+});
+
+app.post('/api/sample', function(req, res) {
+	atrb.create({
+			_id: req.body._id,
+			name: req.body.name,
+			done: true
+	}, function(err, row) {
+		if(err) {
+			res.send(err);
+		} else {
+			atrb.find(function(err, newRow) {
+				if(err) {
+					res.send(err);
+				} else {
+					res.json(newRow);
+				}
+			});
+		}
+	});
+});
+
+app.delete('/api/sample/:dictRowId', function(req, res) {
+	atrb.remove({
+		_id : req.params.dictRowId,
+	}, function(err, row) {
+		if(err) {
+			res.send(err);
+		} else {
+			atrb.find(function(err, newRow) {
+				if(err) {
+					res.send(err);
+				} else {
+					res.json(newRow);
+				}
+			});
+		}
+	});
+});
+
+app.get("*", function(req, res) {
+	res.sendfile('./public/index.html');
+});
 
 app.listen(80);
 console.log("App listening on port 80");
