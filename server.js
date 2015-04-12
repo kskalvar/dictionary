30bc6936fee966692a36c6f0199b1946790acb2a
osistlk <osistlk@gmail.com>
2015-04-11 23:49:28 -0400

    some package.json

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
-{
-	"name" : "dictionary",
-	"version" : "0.0.1",
-	"description" : "Simple html form attribute dictionary",
-	"main" : "server.js",
-	"author" : "f366995",
-	"dependencies" : {
-		"express" : "~4.7.2",
-		"mongoose" : "~3.6.2",
-		"morgan" : "~1.2.2",
-		"body-parser" : "~1.5.2",
-		"method-override" : "~2.1.2"
-	}
-}
+var i = 0;
