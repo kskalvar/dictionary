5be5b2f7e7663da29799396bf13751076a237af5
osistlk <osistlk@gmail.com>
2015-04-12 00:02:51 -0400

    did npm install and wrote server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-var i = 0;
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost//dictionary');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+app.listen(80);
+console.log("App listening on port 80");
